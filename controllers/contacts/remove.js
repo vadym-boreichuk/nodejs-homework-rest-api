9cd761cca4adcc4f85951d48b248c8581390b5de
Vadym Boreichuk <109723184+vadym-boreichuk@users.noreply.github.com>
2023-06-12 14:04:58 +0200

    finish

--- controllers/contacts/remove.js
+++ controllers/contacts/remove.js
@@ -1,19 +1,21 @@
 const createError = require("http-errors");
 const { Contact } = require("../../models/contact");
 
-const remove = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await Contact.findByIdAndDelete(contactId);
-    if (!result) {
-      throw createError(404, `Contact with id=${contactId} not found`);
-    }
-    res.json({
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
+const remove = async (req, res) => {
+  const { _id } = req.user;
+  const { contactId } = req.params;
+
+  const result = await Contact.findOneAndDelete({
+    _id: contactId,
+    owner: _id,
+  });
+
+  if (!result) {
+    throw createError(404, `Contact with id=${contactId} not found`);
   }
+  res.json({
+    message: "contact deleted",
+  });
 };
 
 module.exports = remove;
